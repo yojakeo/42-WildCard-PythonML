21586a2055bedc80246b5c682b362ba45fe98054
Jake <yojakeo99@gmail.com>
2019-09-13 02:07:08 -0700

    WIP entry and output

--- Digit_Recognizion/main.py
+++ Digit_Recognizion/main.py
@@ -1,7 +1,9 @@
 import numpy as np
-from PIL import Image
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import tkinter as tk
+from PIL import Image
+from PIL import ImageTk
 
 data = input_data.read_data_sets("MNIST_data", one_hot=True)  # y labels are oh-encoded
 
@@ -89,7 +91,57 @@ for i in range(n_iterations):
 test_accuracy = sess.run(accuracy, feed_dict={X: data.test.images, Y: data.test.labels, keep_prob: 1.0})
 print("\nAccuracy on test set:", test_accuracy)
 
-img = np.invert(Image.open("test_img2.png").convert('L')).ravel()
+img = np.invert(Image.open("test_img.png").convert('L')).ravel()
 
 prediction = sess.run(tf.argmax(output_layer, 1), feed_dict={X: [img]})
 print ("Prediction for test image:", np.squeeze(prediction))
+
+# Window
+
+# Colour Config
+fg = '#839496'
+bg = '#242428'
+
+gui = tk.Tk()
+
+gui.title("Wildcard 42 ML")
+
+gui.geometry('450x600')
+gui.configure(background=bg)
+
+img_path = 'test_img.png'
+
+def Predict_num(path):
+    img = np.invert(Image.open(path).convert('L')).ravel()
+    num_res = sess.run(tf.argmax(output_layer, 1), feed_dict={X: [img]})
+    np.squeeze(num_res)
+    tk.Label(output_frame, text=str(num_res)).place(x=50, y=40)
+
+# def Open_num_image(img_path):
+
+# Number Image
+img_frame = tk.Frame(bd=5, relief='sunken', bg=bg, height=210, width=210)
+img_frame.place(x=5, y=5)
+width = 200
+height = 200
+img = Image.open(img_path)
+img = img.resize((width,height))
+photoImg =  ImageTk.PhotoImage(img)
+num_image = tk.Label(img_frame, image=photoImg)
+num_image.grid(row=0, column=0)
+
+# Input
+input_frame = tk.Frame(bd=5, bg=bg, height=90, width=210) # Frame
+input_frame.place(x=230, y=5)
+l1 = tk.Label(input_frame, text='Select Image Location', fg=fg, bg=bg) # Label
+l1.place(x=20, y=5)
+e1 = tk.Entry(input_frame,text='') # Entry
+e1.place(x=0, y=30)
+tk.Button(input_frame,text='open').place(x=75, y=60) # Open Command
+
+# Output
+output_frame = tk.Frame(bg=bg, height=70, width=350) # Frame
+output_frame.place(x=0, y=220)
+tk.Button(output_frame, text='Predict', command=Predict_num(img_path), bg=bg, fg=fg).place(x=85, y=5)
+tk.Label(output_frame, text='Predicted Number:', bg=bg, fg=fg).place(x=5, y=40)
+gui.mainloop()
--- Digit_Recognizion/mlwindow.py
+++ Digit_Recognizion/mlwindow.py
@@ -2,45 +2,53 @@ import tkinter as tk
 from PIL import Image
 from PIL import ImageTk
 
+# Colour Config
 fg = '#839496'
 bg = '#242428'
+
 gui = tk.Tk()
 
 gui.title("Wildcard 42 ML")
 
-gui.geometry('600x600')
+gui.geometry('450x600')
 gui.configure(background=bg)
 
-# def Open_num_image(img_path):
+# def Predict_num(path):
+#     img = np.invert(Image.open(path).convert('L')).ravel()
+#     num_res = sess.run(tf.argmax(output_layer, 1), feed_dict={X: [img]})
+#     np.squeeze(num_res)
+#     tk.Label(output_frame, text=str(num_res))
 
+def Open_num_image():
+    string = e1.get()
+    width = 200
+    height = 200
+    img = Image.open(string)
+    img = img.resize((width, height))
+    photo_img = ImageTk.PhotoImage(img)
+    num_image = tk.Label(img_frame, image=photo_img)
+    num_image.grid(row=0, column=0)
+    gui.update_idletasks()
 
-l1 = tk.Label(gui, text='Select Image Location', fg=fg, bg=bg)
-l1.grid(row=0, column=0)
-e1 = tk.Entry(gui,text='')
-e1.grid(row=0, column=1)
+# Number Image
+img_frame = tk.Frame(bd=5, relief='sunken', bg=bg, height=210, width=210)
+img_frame.place(x=5, y=5)
 
-tk.Button(gui,text='open').grid(row=0, column=2)
+# Input
+input_frame = tk.Frame(bd=5, bg=bg, height=90, width=210) # Frame
+input_frame.place(x=230, y=5)
+l1 = tk.Label(input_frame, text='Select Image Location', fg=fg, bg=bg) # Label
+l1.place(x=20, y=5)
+canvas1 = tk.Canvas(input_frame, width=100, height=100)
+canvas1.place(x=0, y=30)
+e1 = tk.Entry(gui)
+canvas1.create_window(200, 140, window=e1)
+tk.Button(input_frame, text='open', command=Open_num_image()).place(x=75, y=60) # Open Command
 
-box1 = tk.Listbox(gui,selectmode='multiple', fg=fg, bg=bg)
-box1.grid(row=10, column=0)
-tk.Button(gui, text='Clear All').grid(row=12,column=0)
-
-box2 = tk.Listbox(gui, fg=fg, bg=bg)
-box2.grid(row=10, column=1)
-tk.Button(gui, text='Select X', fg=fg, bg=bg).grid(row=12,column=1)
-
-T = tk.Text(gui, height=2, width=30, fg=fg, bg=bg)
-T.grid(row=3, column=5)
-T.insert(tk.END, "Just a text Widget\nin two lines\n")
-width = 200
-height = 200
-img = Image.open("test_img.png")
-img = img.resize((width,height))
-photoImg =  ImageTk.PhotoImage(img)
-num_image = tk.Label(gui, image=photoImg)
-num_image.grid(row=8, column=0)
-tk.Button(gui, text='Select y').grid(row=12,column=2)
-
-tk.Button(gui, text='Solution').grid(row=20, column=1)
+# Output
+output_frame = tk.Frame(bg=bg, height=70, width=350) # Frame
+output_frame.place(x=0, y=220)
+tk.Button(output_frame, text='Predict', bg=bg, fg=fg).place(x=85, y=5)
+tk.Label(output_frame, text='Predicted Number:', bg=bg, fg=fg).place(x=5, y=40)
 
 gui.mainloop()
