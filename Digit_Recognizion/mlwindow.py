f4286f7d962a43bf43ed59a9448cd6764023eb84
Jake <yojakeo99@gmail.com>
2019-09-13 17:04:47 -0700

    Finished GUI

--- Digit_Recognizion/mlwindow.py
+++ Digit_Recognizion/mlwindow.py
@@ -10,7 +10,7 @@ gui = tk.Tk()
 
 gui.title("Wildcard 42 ML")
 
-gui.geometry('450x600')
+gui.geometry('450x400')
 gui.configure(background=bg)
 
 # def Predict_num(path):
@@ -18,32 +18,35 @@ gui.configure(background=bg)
 #     num_res = sess.run(tf.argmax(output_layer, 1), feed_dict={X: [img]})
 #     np.squeeze(num_res)
 #     tk.Label(output_frame, text=str(num_res))
-
-def Open_num_image():
-    string = e1.get()
-    width = 200
-    height = 200
-    img = Image.open(string)
-    img = img.resize((width, height))
-    photo_img = ImageTk.PhotoImage(img)
-    num_image = tk.Label(img_frame, image=photo_img)
-    num_image.grid(row=0, column=0)
-    gui.update_idletasks()
+#     gui.update_idletasks()
 
 # Number Image
 img_frame = tk.Frame(bd=5, relief='sunken', bg=bg, height=210, width=210)
 img_frame.place(x=5, y=5)
 
+
+def Open_num_image():
+    e1_out = e1_thing.get()
+    width = 200
+    height = 200
+    img = Image.open(e1_out)
+    img = img.resize((width, height))
+    photo_img = ImageTk.PhotoImage(img)
+    num_image = tk.Label(img_frame, image=photo_img)
+    num_image.image = photo_img
+    num_image.grid(row=0, column=0)
+    gui.update_idletasks()
+
+
 # Input
-input_frame = tk.Frame(bd=5, bg=bg, height=90, width=210) # Frame
+input_frame = tk.Frame(bd=5, bg=bg, height=120, width=210) # Frame
 input_frame.place(x=230, y=5)
 l1 = tk.Label(input_frame, text='Select Image Location', fg=fg, bg=bg) # Label
-l1.place(x=20, y=5)
-canvas1 = tk.Canvas(input_frame, width=100, height=100)
-canvas1.place(x=0, y=30)
-e1 = tk.Entry(gui)
-canvas1.create_window(200, 140, window=e1)
-tk.Button(input_frame, text='open', command=Open_num_image()).place(x=75, y=60) # Open Command
+l1.place(x=20, y=0)
+e1_thing = tk.StringVar()
+e1 = tk.Entry(input_frame, textvariable=e1_thing)
+e1.place(x=0, y=30)
+tk.Button(input_frame, text='open', command=Open_num_image, fg=fg).place(x=75, y=70) # Open Command
 
 # Output
 output_frame = tk.Frame(bg=bg, height=70, width=350) # Frame
